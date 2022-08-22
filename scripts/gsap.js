36e71fc134234fc37555096f096f13de11f6840f
Zuzanna <raf.zuzia7@gmail.com>
2022-08-22 14:02:48 +0200

    feat: added div for phone in red section

--- scripts/gsap.js
+++ scripts/gsap.js
@@ -1,1 +1,4 @@
-gsap.fromTo( "#baloon-element",{x: 300, y: 300,  rotation: -30}, {x: 0, y: 0, duration: 2, rotation: -0, ease: "back.out(1.7)" }, );
+
+gsap.registerPlugin(ScrollTrigger);
+gsap.fromTo("#baloon-element", { x: 300, y: 300, rotation: -90 }, { x: 0, y: 0, duration: 4, rotation: -0, ease: "back.out(3)" },);
+
